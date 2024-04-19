3532d43e248ef531c59c9d0140af4f7868acd305
Josué Clément <josue.d.clement@gmail.com>
2024-04-19 18:58:46 +0200

    Add navigation buttons

--- CodeGenerator/ViewModel/MainWindowViewModel.cs
+++ CodeGenerator/ViewModel/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using Carbon.Services.Interfaces;
 using CodeGenerator.View.Pages;
 using CodeGenerator.ViewModel.Pages;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CodeGenerator.ViewModel;
@@ -10,8 +11,6 @@ namespace CodeGenerator.ViewModel;
 public class MainWindowViewModel : ObservableValidator
 {
     private readonly IServiceProvider _serviceProvider;
-    
-    #region Constructor
 
     public MainWindowViewModel(
         INavigationService navigationService,
@@ -24,13 +23,19 @@ public class MainWindowViewModel : ObservableValidator
         NavigationService.ContentTemplateSelector.RegisterView<PageTemplatesViewModel, PageTemplatesView>();
 
         NavigationService.CurrentViewModel = _serviceProvider.GetRequiredService<PageTemplatesViewModel>();
+
+        NavigateToPageTemplatesCommand = new RelayCommand(NavigateToPageTemplates);
+        NavigateToPageGenerationCommand = new RelayCommand(NavigateToPageGeneration);
     }
-    
-    #endregion
-    
-    #region Properties
 
     public INavigationService NavigationService { get; }
 
-    #endregion
+    public RelayCommand NavigateToPageTemplatesCommand { get; }
+    public RelayCommand NavigateToPageGenerationCommand { get; }
+
+    private void NavigateToPageTemplates()
+        => NavigationService.CurrentViewModel = _serviceProvider.GetRequiredService<PageTemplatesViewModel>();
+
+    private void NavigateToPageGeneration()
+        => NavigationService.CurrentViewModel = _serviceProvider.GetRequiredService<PageGenerationViewModel>();
 }
