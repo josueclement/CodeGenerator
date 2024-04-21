e84ef48a3164ef43ddd4100adf984c0b4d48402a
Josué Clément <josue.d.clement@gmail.com>
2024-04-21 11:48:38 +0200

    Work in progress: Page generation

---
+++ CodeGenerator/Model/CodeTemplate.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeGenerator.Model;
+
+public class CodeTemplate
+{
+    public CodeTemplate(string name, string template, string example)
+    {
+        Name = name;
+        Template = template;
+        Example = example;
+    }
+    
+    public string Name { get; set; }
+    public string Template { get; set; }
+    public string Example { get; set; }
+}
--- CodeGenerator/ViewModel/Pages/PageGenerationViewModel.cs
+++ CodeGenerator/ViewModel/Pages/PageGenerationViewModel.cs
@@ -1,6 +1,53 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using CodeGenerator.Model;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
 namespace CodeGenerator.ViewModel.Pages;
 
-public class PageGenerationViewModel
+public class PageGenerationViewModel : ObservableValidator
 {
+    public PageGenerationViewModel()
+    {
+        GenerateCommand = new RelayCommand(Generate);
+    }
+
+    public string Workspace
+    {
+        get => _workspace;
+        set => SetProperty(ref _workspace, value);
+    }
+    private string _workspace = String.Empty;
     
+    public ObservableCollection<CodeTemplate> Templates { get; } = [];
+
+    public CodeTemplate? SelectedTemplate
+    {
+        get => _selectedTemplate;
+        set => SetProperty(ref _selectedTemplate, value);
+    }
+    private CodeTemplate? _selectedTemplate;
+
+    public string Input
+    {
+        get => _input;
+        set => SetProperty(ref _input, value);
+    }
+    private string _input = string.Empty;
+
+    public string Output
+    {
+        get => _output;
+        set => SetProperty(ref _output, value);
+    }
+    private string _output = string.Empty;
+    
+    public ICommand GenerateCommand { get; }
+
+    private void Generate()
+    {
+        
+    }
 }
